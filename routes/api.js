335ee498739326a35c9391b1fb88e8c3b91fbc29
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2022-11-16 16:37:10 +0100

    back

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,12 @@ router.post("/search", async function (req, res, next) {
   let youtubeAPI = new YoutubeAPI();
   let object = await youtubeAPI.getSongs(req.body.song);
   let songs = await object.items;
+  req.cookies.songs = songs;
+  res.send(JSON.stringify(songs));
+});
+
+router.post("/selected", function (req, res, next) {
+  let songs = req.cookies.songs;
   res.send(JSON.stringify(songs));
 });
 
