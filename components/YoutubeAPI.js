dda87dfca24348af95970d8f617c4eaec276b004
Raúl Torres Alarcón <raultorres2603@gmail.com>
2022-11-21 11:56:11 +0100

    Licencia de video - CC

--- components/YoutubeAPI.js
+++ components/YoutubeAPI.js
@@ -4,10 +4,11 @@ class YoutubeAPI {
   constructor() {
     this.apiKEY = "AIzaSyAeivme_yi0vF8jR0pkvwcFRBJXP1vhsII";
     this.resultsPerPage = 25;
+    this.filters = `type=video&part=snippet&maxResults=${this.resultsPerPage}&safeSearch=strict&videoLicense=creativeCommon`;
   }
 
   async getSongs(title) {
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${this.apiKEY}&type=video&part=snippet&q=${title}&maxResults=${this.resultsPerPage}`;
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${this.apiKEY}&q=${title}&${this.filters}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
